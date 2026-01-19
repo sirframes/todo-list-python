98eea37f90ad3f454054770dc8cb50664dbff6a5
sirframes <alessander.wermuth@gmail.com>
2026-01-19 19:09:14 -0300

    now user can add tasks on GUI version

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 This application is a simple to-do list meant as program practicing, only documentation and text searches are allowed. NO AI.
 A to-do list does not sound exciting, but the journey with no tutorials is tougher than one would expect.
+This project doubles down as my introduction to version control.
 
-Being in doubt between Java and Python, the language I ended up choosing to practice first was python, wich comes bundled with two useful modules for the express
+Being in doubt between Java and Python, the language I choose to practice first was python, which comes bundled with two useful modules for the express
 purpose of this kind of app, tkinter (for GUI's) and sqlite3 (data storage/retrival).
 
 My first challenge after setting things up and having a way to receive data (through the terminal, no GUI yet) was how to modify specific data on demand. 
@@ -12,3 +13,9 @@ became
 #dbCursor.execute("CREATE TABLE IF NOT EXISTS tasks(id INTEGER PRIMARY KEY, name TEXT NOT NULL, state INT NOT NULL DEFAULT 0)")
 
 After making the user able to fetch written tasks and mark them as done, work began on making the GUI.
+
+Keeping references for each widget has proven to be quite challenging, but using arrays to store the widgets during their creation worked wonders for referencing them.
+
+The tasks and extra option buttons have been separated to make things more organized. "lightblue" bg color is used to debug where frames are.
+
+to make a way for the user to add new tasks, I had to choose between a function that forgets all widgets then rebuilds the list with a new entry, or somehow appends the new entry to the list. The later was chosen because rebuilding the list just to update values seems wasteful.
--- main.py
+++ main.py
@@ -1,28 +1,14 @@
 import sqlite3
 import os
 import tkinter as tk
-from tkinter import messagebox, ttk#, font, PhotoImage
-
-# Creating a path to application relative to main.py
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-# Construct full absolute path to the database inside folder db
-db_path = os.path.join(BASE_DIR, 'db', 'todo.db')
-
-#print("The database directory: " + db_path)
-
-# Connecting and starting cursor
-dbConn = sqlite3.connect(db_path)
-dbCursor = dbConn.cursor()
-
-# Make sure the table checks if it exists every time program is opened
-dbCursor.execute("CREATE TABLE IF NOT EXISTS tasks(id INTEGER PRIMARY KEY, name TEXT NOT NULL, state INT NOT NULL DEFAULT 0)")
+from tkinter import ttk#, messagebox, font, PhotoImage
 
 def print_db_rows():
-    state = ""
     querry = dbCursor.execute("SELECT * from tasks")
     if len(querry.fetchall()) < 1:
         print("There are no tasks to show yet.")
         return
+    state = ""
     for row in dbCursor.execute("SELECT * from tasks"):
         if row[2] == 1:
             state = "(done)"
@@ -77,35 +63,6 @@ def register_task(taskAtHand):
         dbConn.commit()
         print("Task successfully registered.")
 
-def main():
-    print("Program entered.")
-    currentinput = '0'    
-    print("Type:\n1 - read all tasks inside the database\n2 - add task\n3 - flip done/undone\nexit - closes program")
-    dbConnState, dbConnStateMessage = check_db_open()
-    while currentinput != 'exit' and dbConnState == True:
-        currentinput = input("\n> ").strip()
-
-        # Print all rows.
-        if currentinput == '1':
-            print_db_rows()
-
-        # Write a new task.
-        elif currentinput == '2':
-            print(dbConnStateMessage)
-            taskAtHand = input("Write a new task. Write 1 to cancel.\n>").strip()
-            register_task(taskAtHand)
-
-        # Flip task between done and undone.
-        elif currentinput == '3':
-            selectId = input("Select a task index to flip between done and undone:\n>")
-            flip_done_undone(selectId)
-
-        # If no available commands are given.
-        elif currentinput != 'exit':
-            print("Type:\n1 - read all tasks inside the database\n2 - add task\n3 - flip done/undone\nexit - closes program")
-    dbConn.close()
-    print("Connection has been closed.\nProgram exited.")
-
 def flip_text_label(label):
 # label = where the text label that needs to change in that row is.
     if label["text"] == "(done)":
@@ -118,67 +75,144 @@ def wrapped_functions_GUI_button(internalTaskID, label):
     flip_done_undone(internalTaskID)
     flip_text_label(label)
 
-class MyGUI:
-    def __init__(self):
-        self.root = tk.Tk()
-        self.root.title("To-do list")
-        #def print_db_rows_GUI():
-        querry = dbCursor.execute("SELECT * from tasks")
-        if len(querry.fetchall()) < 1:
-            print("There are no tasks to show yet.")
-            return
-        state = ""
-        index = 0
-        # Lists needed to directly access tkinter widgets generated in the loop
-        labels = []
-        buttons = []
+def get_database_items():
+    dbCursor.execute("SELECT * from tasks")
+    querry = dbCursor.fetchall()
+    if len(querry) < 1:
+        print("There are no tasks to show yet.")
+    return querry
 
-        for row in dbCursor.execute("SELECT * from tasks"):
-            # 1 means done 0 means to-do
-            if row[2] == 1:
-                state = "(done)"
-            else:
-                state = "(to-do)"
-            #row[0] is the internal id of the task. Index is a tidier number ordering system.
-            self.label = tk.Label(self.root, text=str(index + 1))
+def add_entry_GUI_button(entryText, tasks_frame, labels, buttons):
+    # Appends a new entry to the list
+    register_task(entryText)
+    querry = select_last()
+    database_length = len(get_database_items())
+    if querry[2] == 1:
+        state = "(done)"
+    else:
+        state = "(to-do)"
 
-            # the lambda function ensures the value sent to flip_done_undone is the internal id of when the button was created in the loop, not some random static id.
-            self.button = tk.Button(self.root, text=(row[1]), command=lambda value=[row[0], index]: wrapped_functions_GUI_button(value[0], labels[value[1]]))# update live done and undone states
-            buttons.append(self.button)
+    label = tk.Label(tasks_frame, text=database_length) 
+    button = tk.Button(tasks_frame, text=entryText, command=lambda value=database_length: wrapped_functions_GUI_button(value, labels[value - 1]))
+    buttons.append(button)
+    labelState = tk.Label(tasks_frame, text=state)
+    labels.append(labelState)
 
-            self.labelState = tk.Label(self.root, text=state)
-            labels.append(self.labelState)
+    label.grid(column=0, row=database_length, padx=15)
+    button.grid(column=1, row=database_length, sticky='we', pady=5, padx=5)
+    labelState.grid(column=3, row=database_length)
+    # INFO: possible refactor opportunity
 
-            # Creating grid
-            self.content = ttk.Frame(self.root)
-            self.content.grid(column=0, row=index)
-            
-            # Placing the grid and everything inside
-            self.label.grid(column=0, row=index, padx=15)
-            self.button.grid(column=1, row=index, sticky='we', pady=5, padx=5)
-            self.labelState.grid(column=3, row=index)
-            index += 1
+def select_last():
+    dbCursor.execute("SELECT * FROM tasks ORDER BY id DESC LIMIT 1")
+    querry = dbCursor.fetchone()
+    return querry
 
-        # Print data in terminal
-        self.buttonPrint = tk.Button(self.root, text="Print Database", command=print_db_rows)
-        self.buttonPrint.grid(column=1, row=index)
-        # Add entry
-        self.entryAdd = tk.Entry(self.root)
-        self.entryAdd.grid(column=1, row=index+1)
-        # Add button to submit entry
-        self.buttonForEntry = tk.Button(self.root, text="Add entry", command=lambda : print(self.entryAdd.get()))
-        self.buttonForEntry.grid(column=2, row=index+1)
-        # Starting
-        self.root.mainloop()
+def draw_tasks_GUI(querry, root):
+    # Tasks frame
+    tasks_frame = tk.Frame(root, bg="lightblue")
+    tasks_frame.pack()
+    # Defaults
+    state = ""
+    index = 0
+    # Lists to directly access tkinter widgets generated in the loop
+    labels = [] # For done/to-do
+    buttons = [] # Buttons that flip done/to-do
 
-    #dbConn.commit()
-    #dbConn.close()
+    for row in querry:
+        # 1 means done 0 means to-do
+        if row[2] == 1:
+            state = "(done)"
+        else:
+            state = "(to-do)"
+        #row[0] is the internal id of the task. Index is a tidier number ordering system.
+        label = tk.Label(tasks_frame, text=str(index + 1))
+
+        # the lambda function ensures the value sent to flip_done_undone is the internal id of when the button was created in the loop, not some random static id.
+        button = tk.Button(tasks_frame, text=(row[1]), command=lambda value=[row[0], index]: wrapped_functions_GUI_button(value[0], labels[value[1]]))# update live done and undone states
+        buttons.append(button)
+
+        labelState = tk.Label(tasks_frame, text=state)
+        labels.append(labelState)
+
+        # Creating grid
+        content = ttk.Frame(tasks_frame)
+        content.grid(column=0, row=index)
+        
+        # Placing the grid and everything inside
+        label.grid(column=0, row=index, padx=15)
+        button.grid(column=1, row=index, sticky='we', pady=5, padx=5)
+        labelState.grid(column=3, row=index)
+        index += 1
+
+    # Options frame
+    options_frame = tk.Frame(root)
+    options_frame.pack()
+    # Print data in terminal
+    buttonPrint = tk.Button(options_frame, text="Print Database", command=print_db_rows)
+    buttonPrint.grid(column=1, row=0)
+    # Add entry
+    entryAdd = tk.Entry(options_frame)
+    entryAdd.grid(column=1, row=1)
+    # Add button to submit entry
+    buttonForEntry = tk.Button(options_frame, text="Add entry", command=lambda : add_entry_GUI_button(entryAdd.get().strip(), tasks_frame, labels, buttons))
+    buttonForEntry.grid(column=2, row=1)
+
+def terminal_version():
+    currentinput = '0'    
+    print("Type help for commands or exit to close")
+    dbConnState, dbConnStateMessage = check_db_open()
+    while currentinput != 'exit' and dbConnState == True:
+        currentinput = input("\n> ").strip()
+
+        # Print all rows.
+        if currentinput == '1':
+            print_db_rows()
+
+        # Write a new task.
+        elif currentinput == '2':
+            taskAtHand = input(str(dbConnStateMessage) + "\nWrite a new task. Write 1 to cancel.\n>").strip()
+            register_task(taskAtHand)
+
+        # Flip task between done and undone.
+        elif currentinput == '3':
+            selectId = input("Select a task index to flip between done and undone:\n>")
+            flip_done_undone(selectId)
+
+        # If no available commands are given.
+        elif currentinput == 'help':
+            print("Type:\n1 - read all tasks inside the database\n2 - add task\n3 - flip done/undone\nexit - closes program")
+
+def GUI_version():
+    querry = get_database_items()
+    # Main window
+    root = tk.Tk()
+    root.title("To-do list")
+
+    draw_tasks_GUI(querry, root)
+
+    # Starting
+    root.mainloop()
+
+    #TODO: make temporary buffer for tasks, then dbConn.commit()
+
 # ^^^Function declarations above^^^
-# entering = 0
-# while entering not in("1", "2"):
-#     entering = input("Select 1 for GUI and 2 for terminal: ").strip()
-# if entering == "1":
-#     MyGUI()
-# elif entering == "2":
-#     main()
-MyGUI()
+
+# Creating a path to application relative to main.py
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# Construct full absolute path to the database inside folder db
+db_path = os.path.join(BASE_DIR, 'db', 'todo.db')
+# Connecting and starting cursor
+dbConn = sqlite3.connect(db_path)
+dbCursor = dbConn.cursor()
+# Make sure the table checks if it exists every time program is opened
+dbCursor.execute("CREATE TABLE IF NOT EXISTS tasks(id INTEGER PRIMARY KEY, name TEXT NOT NULL, state INT NOT NULL DEFAULT 0)")
+
+# Entering
+print("Program entered.")
+GUI_version()
+#terminal_version()
+
+# Exiting
+dbConn.close()
+print("Connection has been closed.\nProgram exited.")
